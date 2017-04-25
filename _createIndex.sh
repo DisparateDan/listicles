b33076545ee8effb8929d9cd93f328bec96cf812
Pusceddu <pusceddu@amazon.com>
2017-04-25 15:48:41 -0700

    Fixed better indexing!

--- _createIndex.sh
+++ _createIndex.sh
@@ -2,5 +2,5 @@
 
 # props to https://little418.com/2015/04/directory-listings-on-github-pages.html for the hack!
 
-ls | egrep '(md|png)' | sort | perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"".s/\.md$/\.html/."\">$_</a></li>";} print "</ul></body></html>"' > index.html
+ls | egrep '(md|png)' | sort | perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"". do { (my $tmp = $_) =~ s/\.md$/\.html/; $tmp }."\">$_</a></li>";} print "</ul></body></html>"' > index.html
 
