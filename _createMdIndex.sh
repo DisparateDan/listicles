2dbe30f678de684b2b202cac81f3aba7eb9eaa03
Pusceddu <pusceddu@amazon.com>
2017-04-25 15:59:38 -0700

    Adds md flavor index

---
+++ _createMdIndex.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# props to https://little418.com/2015/04/directory-listings-on-github-pages.html for the original code for the hack!
+
+ls | egrep '(md|png)' | sort | perl -e 'print "# Existing Listicles:\n"; while(<>) { chop $_; print "*[$_](" . do { (my $tmp = $_) =~ s/\.md$/\.html/; $tmp }.")\n";}' > index.md
+
---
+++ index.md
@@ -0,0 +1,5 @@
+# Existing Listicles:
+*[AwesomeGenreBooks.md](AwesomeGenreBooks.html)
+*[KetogenicDiet_101.md](KetogenicDiet_101.html)
+*[README.md](README.html)
+*[index.md](index.html)
